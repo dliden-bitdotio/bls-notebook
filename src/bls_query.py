aa8066e6066ff8f609b4b4540a4ea0e676d62395
dliden-bitdotio <dliden@bit.io>
2022-01-19 12:40:49 -0800

    add industry codes to data pipeline

--- src/bls_query.py
+++ src/bls_query.py
@@ -55,6 +55,7 @@ def build_jolts_dataframe(
     element="QU",
     rate_level="R",
     sa="S",
+    industry="000000",
     start_year=2018,
     end_year=2022,
     name="Quit Rate (Seasonally Adjusted)",
@@ -72,6 +73,8 @@ def build_jolts_dataframe(
         Rate ("R") or Level ("L")
     sa : str
         Seasonally Adjusted ("S") or not ("U")
+    industry : str
+        Industry code (e.g. "000000" for all industries)
     start_year : int
         Starting year of JOLTS data to download
     end_year : int
@@ -90,7 +93,11 @@ def build_jolts_dataframe(
     fips = get_state_fips_codes()
     codes = [
         construct_jolts_id(
-            state=x, element=element, rate_level=rate_level, sa=sa
+            state=x,
+            element=element,
+            rate_level=rate_level,
+            sa=sa,
+            industry=industry,
         )
         for x in fips["state_code"]
     ]
@@ -176,6 +183,146 @@ def build_jolts_dataframe(
     ]
 
 
+def jolts_national_by_industry(
+    registration_key,
+    element,
+    rate_level,
+    sa,
+    name,
+    fips="00",
+    start_year=2018,
+    end_year=2022,
+    annual=True,
+    ):
+    """Download national JOLTS data by industry"""
+
+    industry_codes = {"110000": "Agriculture, Forestry, Fishing and Hunting",
+                      "210000": "Mining, Quarrying, and Oil and Gas Extraction",
+                        "220000": "Utilities",
+                        "230000": "Construction",
+                        "310000": "Manufacturing",
+                        "320000": "Manufacturing",
+                        "330000": "Manufacturing",
+                        "420000": "Wholesale Trade",
+                        "440000": "Retail Trade",
+                        "450000": "Retail Trade",
+                        "480000": "Transportation and Warehousing",
+                        "490000": "Transportation and Warehousing",
+                        "510000": "Information",
+                        "520000": "Finance and Insurance",
+                        "530000": "Real Estate and Rental and Leasing",
+                        "540000": "Professional, Scientific, and Technical Services",
+                        "550000": "Management of Companies and Enterprises",
+                        "560000": "Administrative and Support and Waste Management and Remediation Services",
+                        "610000": "Educational Services",
+                        "620000": "Health Care and Social Assistance",
+                        "710000": "Arts, Entertainment, and Recreation",
+                        "720000": "Accommodation and Food Services",
+                        "810000": "Other Services (except Public Administration)",
+                        "820000": "Public Administration",
+                        }
+
+
+    code_list = industry_codes.keys()
+
+    codes = [
+        construct_jolts_id(
+            element=element,
+            rate_level=rate_level,
+            sa=sa,
+            industry=c,
+            state=fips,
+        )
+        for c in code_list
+    ]
+
+    codes_iter = batch(codes, n=20)
+    # API Call to BLS
+    response_series = []
+    for z in codes_iter:
+        headers = {"Content-type": "application/json"}
+        payload = {
+            "seriesid": z,
+            "startyear": f"{start_year}",
+            "endyear": f"{end_year}",
+        }
+        payload.update({"registrationKey": registration_key})
+        if annual:
+            payload.update({"annualaverage": "true"})
+        payload = json.dumps(payload)
+        response = requests.post(
+            "https://api.bls.gov/publicAPI/v2/timeseries/data/",
+            data=payload,
+            headers=headers,
+        )
+        response.raise_for_status()
+        response_series.extend(response.json()["Results"]["series"])
+
+    # Parse Response into DataFrame
+    dfs = []
+    # Build a pandas series from the API results, bls_series
+    for s in response_series:
+        series_id = s["seriesID"]
+        data = s["data"]
+        data = pd.DataFrame(data)
+        data["series"] = series_id
+        dfs.append(data)
+    df_full = pd.concat(dfs)
+
+    # parse dates
+    df_full["month"] = df_full["period"].str.extract("M(\d+)").astype(int)
+    df_full.loc[df_full["period"] == "M13", "month"] = 1
+    df_full["date"] = pd.to_datetime(df_full[["year", "month"]].assign(day=1))
+    df_full["name"] = name
+    df_full["footnotes"] = df_full["footnotes"].apply(
+        lambda x: x[0].setdefault("text", np.nan)
+    )
+    df_full["seasonally_adjusted"] = sa
+    # df_full['footnotes'] = df_full['footnotes'].apply(lambda x: x[0]['text'])
+    df_full.loc[df_full["period"] == "M13", "date"] = None
+    # parse geography
+    df_full["state_code"] = df_full["series"].str.slice(start=9, stop=11)
+
+    # extract industry
+    df_full["industry_code"] = df_full["series"].str.slice(start=3, stop=9)
+    df_full["industry"] = df_full["industry_code"].map(industry_codes)
+
+
+    # Specify Types
+    df_full = df_full.astype(
+        {
+            "series": "str",
+            "state_code": "str",
+            "year": "int",
+            "date": "datetime64[D]",
+            "name": "str",
+            "footnotes": "str",
+            "seasonally_adjusted": "str",
+            "industry_code": "str",
+            "industry": "str",
+            "value": "float",
+        }
+    )
+    df_full["month"] = df_full["date"].dt.month
+
+    return df_full.loc[
+        :,
+        [
+            "series",
+            "name",
+            "year",
+            "month",
+            "date",
+            "state_code",
+            "seasonally_adjusted",
+            "industry_code",
+            "industry",
+            "value",
+            "footnotes",
+        ],
+    ]
+
+
 def get_recessions_fred(
     api_key, start_date="2003-01-01", end_date="2022-01-11"
 ):
@@ -193,7 +340,12 @@ def get_recessions_fred(
         :, ["date", "value"]
     ]
 
+
 def monthly_national_sub(bls_series, sa="S"):
     monthly = bls_series[bls_series.date.notnull()]
-    monthly_national = monthly.loc[(monthly['state'] == "Total US") & (monthly["seasonally_adjusted"] == sa), :]
+    monthly_national = monthly.loc[
+        (monthly["state"] == "Total US")
+        & (monthly["seasonally_adjusted"] == sa),
+        :,
+    ]
     return monthly_national
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 
 from dotenv import load_dotenv
 
-from bls_query import build_jolts_dataframe
+from bls_query import build_jolts_dataframe, jolts_national_by_industry
 from upload_download_bitdotio import upload_table
 
 if __name__ == "__main__":
@@ -107,3 +107,90 @@ if __name__ == "__main__":
     upload_table(df=quitrate_combined, upload_schema=BITIO_REPO, upload_table=QUIT_RATE_TABLE, bitio_pg_string=PG_STRING)
     upload_table(df=ld_combined, upload_schema=BITIO_REPO, upload_table=LD_TABLE, bitio_pg_string=PG_STRING)
     upload_table(df=openings_combined, upload_schema=BITIO_REPO, upload_table=OPENINGS_TABLE, bitio_pg_string=PG_STRING)
+
+    # Industry Level Data
+    ## quit rates
+    industry_quits_sa = jolts_national_by_industry(
+        element="QU",
+        rate_level="R",
+        sa="S",
+        start_year=2003,
+        end_year=CURRENTYEAR,
+        annual=False,
+        registration_key=BLS_KEY,
+        name="Quit Rate by Industry",
+    )
+    industry_quits_sa["seasonal_adjustment"] = "S"
+
+    industry_quits_u = jolts_national_by_industry(
+        element="QU",
+        rate_level="R",
+        sa="U",
+        start_year=2003,
+        end_year=CURRENTYEAR,
+        annual=False,
+        registration_key=BLS_KEY,
+        name="Quit Rate by Industry",
+    )
+    industry_quits_u["seasonal_adjustment"] = "U"
+
+    industry_quits_combined = industry_quits_sa.append(industry_quits_u)
+
+    ## layoffs & discharges
+    industry_ld_sa = jolts_national_by_industry(
+        element="LD",
+        rate_level="R",
+        sa="S",
+        start_year=2003,
+        end_year=CURRENTYEAR,
+        annual=False,
+        registration_key=BLS_KEY,
+        name="Layoffs and Discharges by Industry",
+    )
+    industry_ld_sa["seasonal_adjustment"] = "S"
+
+    industry_ld_u = jolts_national_by_industry(
+        element="LD",
+        rate_level="R",
+        sa="U",
+        start_year=2003,
+        end_year=CURRENTYEAR,
+        annual=False,
+        registration_key=BLS_KEY,
+        name="Layoffs and Discharges by Industry",
+    )
+    industry_ld_u["seasonal_adjustment"] = "U"
+
+    industry_ld_combined = industry_ld_sa.append(industry_ld_u)
+
+    ## openings
+    industry_openings_sa = jolts_national_by_industry(
+        element="JO",
+        rate_level="R",
+        sa="S",
+        start_year=2003,
+        end_year=CURRENTYEAR,
+        annual=False,
+        registration_key=BLS_KEY,
+        name="Job Openings by Industry",
+    )
+    industry_openings_sa["seasonal_adjustment"] = "S"
+
+    industry_openings_u = jolts_national_by_industry(
+        element="JO",
+        rate_level="R",
+        sa="U",
+        start_year=2003,
+        end_year=CURRENTYEAR,
+        annual=False,
+        registration_key=BLS_KEY,
+        name="Job Openings by Industry",
+    )
+    industry_openings_u["seasonal_adjustment"] = "U"
+
+    industry_openings_combined = industry_openings_sa.append(industry_openings_u)
+
+
+    upload_table(df=industry_quits_combined, upload_schema=BITIO_REPO, upload_table="industry_quits", bitio_pg_string=PG_STRING)
+    upload_table(df=industry_ld_combined, upload_schema=BITIO_REPO, upload_table="industry_ld", bitio_pg_string=PG_STRING)
+    upload_table(df=industry_openings_combined, upload_schema=BITIO_REPO, upload_table="industry_openings", bitio_pg_string=PG_STRING)
